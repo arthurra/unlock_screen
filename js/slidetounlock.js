a521e76d37122609d149582ab0f35d9b3630dc2b
Ryan Arthur <ryan.arthur5819@gmail.com>
2012-11-14 11:25:25 -0500

    add working files

---
+++ js/slidetounlock.js
@@ -0,0 +1,18 @@
+$(function() {
+	$("#slider").draggable({
+		axis: 'x',
+		containment: 'parent',
+		drag: function(event, ui) {
+			if (ui.position.left > 470) {
+				$("body").fadeOut();
+			}
+		},
+		stop: function(event, ui) {
+			if (ui.position.left < 471) {
+				$(this).animate({
+					left: 0
+				})
+			}
+		}
+	});
+});

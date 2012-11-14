068603cd62564f17f1f146eb59067a70404e305e
Ryan Arthur <ryan.arthur5819@gmail.com>
2012-11-14 12:25:21 -0500

    make fadeout function smart

--- js/slidetounlock.js
+++ js/slidetounlock.js
@@ -1,16 +1,18 @@
 $(function() {
-	var w = $(#well).width;
+	var w = $("#well").width();
+	var d = w * 0.75;
 
 	$("#slider").draggable({
 		axis: 'x',
 		containment: 'parent',
 		drag: function(event, ui) {
-			if (ui.position.left > 470) {
+			console.log(ui.position.left);
+			if (ui.position.left > d) {
 				$("body").fadeOut();
 			}
 		},
 		stop: function(event, ui) {
-			if (ui.position.left < 471) {
+			if (ui.position.left < d + 1) {
 				$(this).animate({
 					left: 0
 				})
